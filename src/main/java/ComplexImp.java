4e28bb4b37a038c32036590f4879c3aeca502f06
Igor Gunin <gunin-igor75@yandex.ru>
2023-01-24 15:25:18 +0600

    version 1.0

--- src/main/java/ComplexImp.java
+++ src/main/java/ComplexImp.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class ComplexImp implements Complex {
 
     private final double real;
@@ -14,28 +16,57 @@ public class ComplexImp implements Complex {
     }
 
     @Override
-    public Complex sum(Complex other) {
-        return null;
+    public Complex sum(Complex other) {     // (x1 + iy1) + (x2 + iy2) = (x1 + x2) + (y1 + y2)i
+        double newReal = real + other.getReal();
+        double newImage = image + other.getImage();
+        return new ComplexImp(newReal, newImage);
     }
 
     @Override
-    public Complex sub(Complex other) {
-        return null;
+    public Complex sub(Complex other) {     // (x1 + iy1) - (x2 + iy2) = (x1 - x2) + (y1 - y2)i
+        double newReal = real - other.getReal();
+        double newImage = image - other.getImage();
+        return new ComplexImp(newReal, newImage);
     }
 
     @Override
-    public Complex mul(Complex other) {
-        return null;
+    public Complex mul(Complex other) {     //(x1 + iy1)*(x2 + iy2) = x1x2 - y1y2 + (x1y2 + x2y1)i
+        double newReal = real * other.getReal() -  image * other.getImage();
+        double newImage = real * other.getImage() + other.getReal() * image;
+        return new ComplexImp(newReal, newImage);
     }
 
     @Override
     public double getReal() {
-        return 0;
+        return real;
     }
 
     @Override
     public double getImage() {
-        return 0;
+        return image;
+    }
+
+    @Override
+    public int hashCode() {
+        String realString = String.format("%.2f", real);
+        String imageString = String.format("%.2f", image);
+        return Objects.hash(realString, imageString);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || this.getClass() != obj.getClass()) {
+            return false;
+        }
+        ComplexImp other = (ComplexImp) obj;
+        String realString = String.format("%.2f", real);
+        String imageString = String.format("%.2f", image);
+        String realOther = String.format("%.2f", other.getReal());
+        String imageOther = String.format("%.2f", other.getImage());
+        return realString.equals(realOther) && imageString.equals(imageOther);
     }
 
     @Override
